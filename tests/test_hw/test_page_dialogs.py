ce332a079a4cccebd070fba1f81bacaf38fc9602
timatidallas <dallas_84@mail.ru>
2023-05-15 00:50:28 +0300

    klass_demoqa

--- tests/test_hw/test_page_dialogs.py
+++ tests/test_hw/test_page_dialogs.py
@@ -1,3 +1,5 @@
+import time
+
 from pages.modal_dialogs import ModalDialogs
 
 def test_modal_elements(browser):
@@ -9,9 +11,14 @@ def test_navigation_modal(browser):
     navig_modal = ModalDialogs(browser)
     navig_modal.visit()
     navig_modal.refresh()
+    time.sleep(2)
     navig_modal.main_icon.click()
+    time.sleep(2)
     navig_modal.back()
+    time.sleep(2)
     browser.set_window_size(900, 400)
+    time.sleep(2)
     navig_modal.forward()
+    time.sleep(2)
     # assert navig_modal.equal_url()
-    # assert browser.title == navig_modal.pageData["title"]
+    assert browser.title == 'DEMOQA'
